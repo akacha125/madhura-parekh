138931561a8c347c36aa3ffc8454d5c4d0f39b83
Akhil Parekh <parekhakhil11@gmail.com>
2024-12-25 12:54:13 +0530

    Update portfolio-data.ts

--- src/data/portfolio-data.ts
+++ src/data/portfolio-data.ts
@@ -11,7 +11,7 @@ export const portfolioData = {
     email: "artbymadhura@gmail.com",
     phone: "(+91) 98981 13511",
     social: {
-      instagram: "https://instagram.com/(@art_by_madhura)",
+      instagram: "https://www.instagram.com/(@art_by_madhura)",
       twitter: "https://twitter.com/parekhakhil",
       linkedin: "https://linkedin.com/in/madhuraparekh",
       behance: "https://behance.net/artist",
