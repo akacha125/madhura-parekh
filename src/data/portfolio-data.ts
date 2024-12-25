8a9ac8adbf48b18bd40de6f66a945fde451a1d66
Akhil Parekh <parekhakhil11@gmail.com>
2024-12-25 16:41:44 +0530

    Update portfolio-data.ts

--- src/data/portfolio-data.ts
+++ src/data/portfolio-data.ts
@@ -11,7 +11,7 @@ export const portfolioData = {
     social: {
       instagram: "https://instagram.com/art__by__madhura",
       twitter: "https://twitter.com/parekhakhil",
-      linkedin: "https://linkedin.com/in/madhura-parekh",
+      linkedin: "https://linkedin.com/in/madhura-parekh-j",
       behance: "https://behance.net/artist",
     },
   },
